ff5c247acb556b3570e1c8e7a235f666be0eaea7
Tomek <tomasz@tomaka.eu>
2016-03-12 22:34:16 +0100

    Added spinner with radio channels - switching channels for playback not implemented yet

--- app/src/main/java/eu/tomaka/radiored7/MainActivity.java
+++ app/src/main/java/eu/tomaka/radiored7/MainActivity.java
@@ -4,26 +4,39 @@ package eu.tomaka.radiored7;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private MediaPlayer mp;
     private Button buttonPlay;
     private Button buttonStop;
-
+    private Spinner spinnerRaioChannel;
+    private HashMap<String, String> channelSCAddressHash = new HashMap<String, String>();
+    private String choosenChannel = "Główny";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spinnerRaioChannel = (Spinner) findViewById(R.id.spinnerRaioChannel);
         buttonPlay = (Button) findViewById(R.id.buttonPlay);
         buttonStop = (Button) findViewById(R.id.buttonStop);
         buttonPlay.setOnClickListener(this);
         buttonStop.setEnabled(false);
         buttonStop.setOnClickListener(this);
+        setupShoutcastAddresses();
         initializeMP();
     }
 
@@ -56,8 +69,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void initializeMP(){
         mp = new MediaPlayer();
         try {
-            mp.setDataSource("http://sluchaj.radiors.pl:19182");
+            mp.setDataSource(channelSCAddressHash.get(choosenChannel));
+//            mp.setDataSource("http://sluchaj.radiors.pl:19182");
         } catch (IOException e) {
+            Log.e("Red7", "Unable initialize player with address " + channelSCAddressHash.get(choosenChannel));
             e.printStackTrace();
         }
     }
@@ -89,5 +104,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
         buttonStop.setEnabled(false);
     }
 
+    private void setupShoutcastAddresses(){
+        channelSCAddressHash.put("Główny", "http://sluchaj.radiors.pl:19182");
+        channelSCAddressHash.put("Fly", "http://sluchaj.radiors.pl:19204");
+        channelSCAddressHash.put("Disco-Polo", "http://sluchaj.radiors.pl:19206");
+        List<String> channelList = new ArrayList(channelSCAddressHash.keySet());
+        Collections.sort(channelList,Collections.reverseOrder());
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item,channelList);
+        spinnerRaioChannel.setAdapter(dataAdapter);
+        // Spinner item selection Listener
+        addListenerOnSpinnerItemSelection();
 
+        // Button click Listener
+        addListenerOnButton();
+    }
+    // Add spinner data
+
+    public void addListenerOnSpinnerItemSelection(){
+
+//        spinnerRaioChannel.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    //get the selected dropdown list value
+
+    public void addListenerOnButton() {
+
+
+    }
 }
