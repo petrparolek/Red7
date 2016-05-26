d145aa0a8ef510153c69c2d8eec1ad70b61ccbdd
Tomek <tomasz@tomaka.eu>
2016-05-26 23:37:48 +0200

    changed design of play/pause button

--- app/build/generated/source/r/debug/eu/tomaka/radiored7/R.java
+++ app/build/generated/source/r/debug/eu/tomaka/radiored7/R.java
@@ -2245,16 +2245,12 @@ containing a value of this type.
         public static final int design_fab_background=0x7f020049;
         public static final int design_snackbar_background=0x7f02004a;
         public static final int drop=0x7f02004b;
-        public static final int ic_pause_circle_outline_black_48dp=0x7f02004c;
-        public static final int ic_pause_circle_outline_white_48dp=0x7f02004d;
-        public static final int ic_play_circle_outline_black_48dp=0x7f02004e;
-        public static final int ic_play_circle_outline_white_48dp=0x7f02004f;
-        public static final int notification_template_icon_bg=0x7f020055;
-        public static final int pause=0x7f020050;
-        public static final int play=0x7f020051;
-        public static final int red7logo=0x7f020052;
-        public static final int spinner=0x7f020053;
-        public static final int spinner_triangle=0x7f020054;
+        public static final int notification_template_icon_bg=0x7f020051;
+        public static final int pause=0x7f02004c;
+        public static final int play=0x7f02004d;
+        public static final int red7logo=0x7f02004e;
+        public static final int spinner=0x7f02004f;
+        public static final int spinner_triangle=0x7f020050;
     }
     public static final class id {
         public static final int action0=0x7f0c007a;
